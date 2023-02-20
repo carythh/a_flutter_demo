62ef5c45a1c62cbe4d445184034473889d060d21
tan huohui <carythh@users.noreply.github.com>
2023-02-20 19:57:12 +0800

    feat: add json convert  and add user entity

---
+++ lib/mine/models/user_entity.dart
@@ -0,0 +1,18 @@
+import '../../generated/json/base/json_field.dart';
+import '../../generated/json/user_entity.g.dart';
+
+@JsonSerializable()
+class UserEntity {
+
+  UserEntity();
+
+  factory UserEntity.fromJson(Map<String, dynamic> json) => $UserEntityFromJson(json);
+
+  Map<String, dynamic> toJson() => $UserEntityToJson(this);
+
+  @JSONField(name: 'avatar_url')
+  String? avatarUrl;
+  String? name;
+  int? id;
+  String? blog;
+}
