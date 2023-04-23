a98c77f577f6a30e80bde85efa9f716b129359b5
tan huohui <carythh@users.noreply.github.com>
2023-04-23 18:11:47 +0800

    feat
    add keyboard action and url launcher plugins

--- lib/login/page/login_page.dart
+++ lib/login/page/login_page.dart
@@ -6,8 +6,10 @@ import '../../res/colors.dart';
 import '../../res/constant.dart';
 import '../../res/gaps.dart';
 import '../../util/change_notifier_manage.dart';
+import '../../util/other_utils.dart';
 import '../../widgets/my_app_bar.dart';
 import '../../widgets/my_button.dart';
+import '../../widgets/my_scroll_view.dart';
 import '../widget/my_text_field.dart';
 
 /// 实现Login Page，继承自StatefulWidget ，两个输入框手机号、验证码，一个获取验证码按钮，点击获取验证吗提示60秒倒计时，获取验证码按钮不可点击  ，一个登录按钮，点击登录按钮验证手机号是否正确，验证是否输入了验证码，验证通过后跳转到主页
@@ -36,22 +38,22 @@ class _LoginPageState extends State<LoginPage>  with ChangeNotifierMixin<LoginPa
 
   @override
   Widget build(BuildContext context) {
-    // return Scaffold(
-    //   appBar: MyAppBar(
-    //     isBack: false,
-    //     // actionName: DeerLocalizations.of(context)!.verificationCodeLogin,
-    //     actionName: "getVerificationCode",
-    //     onPressed: () {
-    //      // NavigatorUtils.push(context, LoginRouter.smsLoginPage);
-    //     },
-    //   ),
-    //   body: MyScrollView(
-    //     keyboardConfig: Utils.getKeyboardActionsConfig(context, <FocusNode>[_nodeText1, _nodeText2]),
-    //     padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 20.0),
-    //     children: _buildBody,
-    //   ),
-    // );
-    return Container();
+    return Scaffold(
+      appBar: MyAppBar(
+        isBack: false,
+        // actionName: DeerLocalizations.of(context)!.verificationCodeLogin,
+        actionName: "getVerificationCode",
+        onPressed: () {
+         // NavigatorUtils.push(context, LoginRouter.smsLoginPage);
+        },
+      ),
+      body: MyScrollView(
+        keyboardConfig: Utils.getKeyboardActionsConfig(context, <FocusNode>[_nodeText1, _nodeText2]),
+        padding: const EdgeInsets.only(left: 16.0, right: 16.0, top: 20.0),
+        children: _buildBody,
+      ),
+    );
+   // return Container();
   }
 
   ///两个输入框手机号、验证码，一个获取验证码按钮，点击获取验证吗提示60秒倒计时，获取验证码按钮不可点击  ，一个登录按钮，点击登录按钮验证手机号是否正确，验证是否输入了验证码
